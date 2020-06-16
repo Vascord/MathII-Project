e25a1dc5c4d4fba34b8c2d7ac410d160e10f9e33
João Borges <joaonunos.borges@gmail.com>
2020-06-16 02:21:42 +0100

    Viscosidade turbulenta

--- MathII-Project.py
+++ MathII-Project.py
@@ -6,12 +6,15 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((640,480))
 screen.fill((255, 255, 255))
 
-degrees = 30.0
-speed = 30.0
+degrees = 60.0
+speed = 100.0
 speedX = 0
 speedY = 0
-viscosity = 1.0
-gravity = 9.81
+viscosity = 0.2
+viscosityType = "laminar"
+gravity = -9.8
+directionX = 0
+directionY = 0
 mass = 10.0
 forces = []
 timestep = 0.02
@@ -19,9 +22,6 @@ accelX = 0
 accelY = 0
 Px = 0
 Py = 0
-
-def map_generator():
-    return
 
 
 while(True):
@@ -31,27 +31,36 @@ while(True):
             exit()
 
     print("Firing at", degrees, "degrees, at", speed,
-        "m/s.\nViscosity is "+ str(viscosity) +", gravity is " + str(gravity) +
+        "m/s.\nViscosity is "+ str(viscosity) +" , type is" + str(viscosityType) + ", gravity is " + str(gravity) +
         ".\nThe mass of the object is " + str(mass) + ".")
 
-    #Meter aqui funçao que faz que o mapa se actualize
     Px = 30
     Py = 429
-    speedX = speed * math.sin(math.radians(degrees))
+    speedX = speed * math.cos(math.radians(degrees))
     speedY = -(speed * math.sin(math.radians(degrees)))
+    directionX = speedX / abs(speed)
+    directionY = speedY / abs(speed)
+
     for i in range(len(forces)):
         accelX += forces[i][0] / mass
         accelY += forces[i][1] / mass
-    while((Py <= 430) and (Py > -1000)):
-        #Perguntar ao stor se as forças são constantes(provavelmente) ou de impulso
-        Px += speedX * timestep + 0.5*(-viscosity * speedX + accelX) * (timestep**2)
-        Py += speedY * timestep + 0.5*(gravity - viscosity * speedY - accelY) * (timestep**2)
-        # massa afeta acelaração do cociente de viscosidade? Tenho quase a certeza que não pq é uma força de resposta à velocidade
-        # e se houver massa envolvida também esta do lado a que desacelera mas melhor verificar
-        speedX += 0.5*(-viscosity * speedX + accelX) * timestep
-        speedY += 0.5*(gravity - viscosity * speedY - accelY) * timestep
+    while((Py <= 430) and (Py > -500)):
+        if (viscosityType == "laminar"):
+            Px += speedX * timestep + 0.5*(-viscosity * speedX + accelX) * (timestep**2)
+            Py += speedY * timestep + 0.5*(-gravity - viscosity * speedY - accelY) * (timestep**2)
 
-        pygame.draw.circle(screen, (0,0,0), (int(Px),int(Py)), 1)
+            speedX += (-viscosity * speedX + accelX) * timestep
+            speedY += (-gravity - viscosity * speedY - accelY) * timestep
+
+            pygame.draw.circle(screen, (0,0,0), (int(Px),int(Py)), 1)
+        elif (viscosityType == "turbulent"):
+            Px += speedX * timestep + 0.5*(-viscosity * directionX * (speed**2) + accelX) * (timestep**2)
+            Py += speedY * timestep + 0.5*(-gravity - viscosity * directionY * (speed**2) - accelY) * (timestep**2)
+
+            speedX += (-viscosity * directionX * (speed**2) + accelX) * timestep
+            speedY += (-gravity - viscosity * directionY * (speed**2) - accelY) * timestep
+
+            pygame.draw.circle(screen, (0,0,0), (int(Px),int(Py)), 1)
 
     pygame.draw.circle(screen, (255,0,0), (int(Px),int(Py)), 3)
     pygame.display.flip()
@@ -76,6 +85,10 @@ while(True):
                 viscosity = abs(float(command[14:]))
                 screen.fill((255, 255, 255))
 
+            elif(command[4:8] == "type"):
+                viscosityType = str(command[8:].strip())
+                screen.fill((255, 255, 255))
+
             elif(command[4:11] == "gravity"):
                 gravity = abs(float(command[12:]))
                 screen.fill((255, 255, 255))
@@ -86,13 +99,13 @@ while(True):
 
             else:
                 print("invalid option")
-        
+
         elif(command[0:6] == "forces"):
             i = 0
             for force in forces:
                 print(str(i) + ". F = (" , force[0] ,",", force[1] , ")")
                 i += 1
-        
+
         elif(command[0:8] == "addforce"):
             space = 0
             command_space = 9
@@ -107,15 +120,15 @@ while(True):
 
                 else:
                     command_space += 1
-            
+
             if(error == 1):
                 print("invalid option")
 
         elif(command[0:11] == "removeforce"):
             forces.pop(int(command[12:]))
             screen.fill((255, 255, 255))
-            
-        
+
+
         elif(command == "quit"):
             break
 
@@ -124,6 +137,4 @@ while(True):
 
     except:
         print("invalid option")
-    
-    pygame.display.flip()
 
